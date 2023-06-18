d66d907c0507932980b2075c1910dab422bcad1b
Juan Camilo Narváez <ulvenforst@gmail.com>
2023-06-18 11:30:44 -0500

    Se implementó el ControladorLogin para los usuarios, falta diferenciar entre empleados y clientes

--- src/main/java/co/edu/univalle/Main.java
+++ src/main/java/co/edu/univalle/Main.java
@@ -16,12 +16,39 @@
 */
 package co.edu.univalle;
 
+import co.edu.univalle.vistas.vistaLogin;
 import co.edu.univalle.vistas.vistaUConsultarLibro;
 
-public class Main {
+import java.sql.Connection;
 
+import co.edu.univalle.controlador.ControladorLogin;
+import co.edu.univalle.persistencia.Biblioteca;
+import co.edu.univalle.persistencia.DaoContrasenaUsuario;
+import co.edu.univalle.persistencia.DaoUsuario;
+import co.edu.univalle.persistencia.FachadaBD;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        vistaUConsultarLibro prueba = new vistaUConsultarLibro("Consultar libro");
+        String url = "jdbc:postgresql://localhost:5432/biblioteca_database";
+        String usuario = "ulven";
+        String password = "Ulven_1729";
+
+        FachadaBD fachadaBD = new FachadaBD(url, usuario, password);
+        Connection conexionBD = fachadaBD.getConexionBD();
+
+        // Crea las instancias DAO para el usuario y la contraseña
+        DaoUsuario daoUsuario = new DaoUsuario(conexionBD);
+        DaoContrasenaUsuario daoContrasenaUsuario = new DaoContrasenaUsuario(conexionBD);
+        
+        // Crea la vista
+        vistaLogin vistaLogin = new vistaLogin("Inicio Sesión");
+        
+        // Crea el controlador pasando las instancias DAO creadas
+        ControladorLogin controladorLogin = new ControladorLogin(vistaLogin, daoUsuario, daoContrasenaUsuario);
+        
+        // Inicializa la vista
+        vistaLogin.setVisible(true);
     }
 }
+
+
