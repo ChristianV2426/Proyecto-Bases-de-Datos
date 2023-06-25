3dcd182974d43201b6dbcc6d21ad3677045cb8f7
Christian Vargas <vargas.christian@correounivalle.edu.co>
2023-06-24 20:49:55 -0500

    Añadí lógica para obtener consecutivos actualizados con la base de datos

--- src/main/java/co/edu/univalle/persistencia/Biblioteca.java
+++ src/main/java/co/edu/univalle/persistencia/Biblioteca.java
@@ -41,6 +41,13 @@ public class Biblioteca {
   private DaoPrestamo prestamos;
   private DaoRelacionPresta relacionesPresta;
   private DaoMulta multas;
+  private Integer serialLibro;
+  private Integer serialUsuario;
+  private Integer serialEmpleado;
+  private Integer serialSolicitud;
+  private Integer serialDescarga;
+  private Integer serialPrestamo;
+  private Integer serialMulta;
 
   public Biblioteca(Connection conexionBD) {
     this.conexionBD = conexionBD;
@@ -64,6 +71,54 @@ public class Biblioteca {
     this.prestamos = new DaoPrestamo(conexionBD);
     this.relacionesPresta = new DaoRelacionPresta(conexionBD);
     this.multas = new DaoMulta(conexionBD);
+    this.inicializarSeriales();
+  }
+
+  public void inicializarSeriales() {
+    String selectSerialLibro = "SELECT MAX(substring(ISBN, 3)) FROM libro;";
+    String selectSeriallUsuario = "SELECT MAX(substring(id_usuario, 3)) FROM usuario;";
+    String selectSerialEmpleado = "SELECT MAX(substring(id_empleado, 3)) FROM empleado;";
+    String selectSerialSolicitu = "SELECT MAX(substring(codigo_solicitud, 3)) FROM solicitud;";
+    String selectSerialDescarga = "SELECT MAX(substring(codigo_descarga, 3)) FROM descarga;";
+    String selectSerialPrestamo = "SELECT MAX(substring(codigo_prestamo, 3)) FROM prestamo;";
+    String selectSerialMulta = "SELECT MAX(substring(codigo_multa, 3)) FROM multa;";
+
+    try{
+      Statement sentenciaSQL = conexionBD.createStatement();
+      
+      ResultSet serialLibroBD = sentenciaSQL.executeQuery(selectSerialLibro);
+      serialLibroBD.next();
+      System.out.println(serialLibroBD.getInt(1));
+      serialLibro = serialLibroBD.getInt(1) + 1;
+
+      ResultSet serialUsuarioBD = sentenciaSQL.executeQuery(selectSeriallUsuario);
+      serialUsuarioBD.next();
+      serialUsuario = serialUsuarioBD.getInt(1) + 1;
+
+      ResultSet serialEmpleadoBD = sentenciaSQL.executeQuery(selectSerialEmpleado);
+      serialEmpleadoBD.next();
+      serialEmpleado = serialEmpleadoBD.getInt(1) + 1;
+
+      ResultSet serialSolicitudBD = sentenciaSQL.executeQuery(selectSerialSolicitu);
+      serialSolicitudBD.next();
+      serialSolicitud = serialSolicitudBD.getInt(1) + 1;
+
+      ResultSet serialDescargaBD = sentenciaSQL.executeQuery(selectSerialDescarga);
+      serialDescargaBD.next();
+      serialDescarga = serialDescargaBD.getInt(1) + 1;
+
+      ResultSet serialPrestamoBD = sentenciaSQL.executeQuery(selectSerialPrestamo);
+      serialPrestamoBD.next();
+      serialPrestamo = serialPrestamoBD.getInt(1) + 1;
+
+      ResultSet serialMultaBD = sentenciaSQL.executeQuery(selectSerialMulta);
+      serialMultaBD.next();
+      serialMulta = serialMultaBD.getInt(1) + 1;
+
+    } catch (Exception error) {
+
+      System.out.println("No se pudo ejecutar la sentencia SELECT para inicializar los seriales de la Base de Datos.\nError: " + error.getMessage());;
+    }
   }
 
   public DaoEditorial getEditoriales() {
@@ -144,6 +199,111 @@ public class Biblioteca {
 
   public DaoMulta getMultas() {
     return multas;
+  }
+
+  public String getSerialLibro() {
+    if(serialLibro < 10)
+      return "LI00" + serialLibro;
+
+    else if (serialLibro < 100)
+      return "LI0" + serialLibro;
+
+    else
+      return "LI" + serialLibro;
+    }
+
+  public String getSerialUsuario() {
+    if(serialUsuario < 10)
+      return "US00" + serialUsuario;
+    
+    else if (serialUsuario < 100)
+      return "US0" + serialUsuario;
+
+    else
+      return "US" + serialUsuario;
+  }
+
+  public String getSerialEmpleado() {
+    if(serialEmpleado < 10)
+      return "EM00" + serialEmpleado;
+    
+    else if (serialEmpleado < 100)
+      return "EM0" + serialEmpleado;
+
+    else
+      return "EM" + serialEmpleado;
+  }
+
+  public String getSerialSolicitud() {
+    if(serialSolicitud < 10)
+      return "CS00" + serialSolicitud;
+    
+    else if (serialSolicitud < 100)
+      return "CS0" + serialSolicitud;
+
+    else
+      return "CS" + serialSolicitud;
+  }
+
+  public String getSerialDescarga() {
+    if(serialDescarga < 10)
+      return "DS00" + serialDescarga;
+    
+    else if (serialDescarga < 100)
+      return "DS0" + serialDescarga;
+
+    else
+      return "DS" + serialDescarga;
+  }
+
+  public String getSerialPrestamo() {
+    if(serialPrestamo < 10)
+      return "PR00" + serialPrestamo;
+    
+    else if (serialPrestamo < 100)
+      return "PR0" + serialPrestamo;
+
+    else
+      return "PR" + serialPrestamo;
+  }
+
+  public String getSerialMulta() {
+    if(serialMulta < 10)
+      return "MU00" + serialMulta;
+    
+    else if (serialMulta < 100)
+      return "MU0" + serialMulta;
+
+    else
+      return "MU" + serialMulta;
+  }
+
+  public void sumarSerialLibro() {
+    serialLibro++;
+  }
+
+  public void sumarSerialUsuario() {
+    serialUsuario++;
+  }
+
+  public void sumarSerialEmpleado() {
+    serialEmpleado++;
+  }
+
+  public void sumarSerialSolicitud() {
+    serialSolicitud++;
+  }
+
+  public void sumarSerialDescarga() {
+    serialDescarga++;
+  }
+
+  public void sumarSerialPrestamo() {
+    serialPrestamo++;
+  }
+
+  public void sumarSerialMulta() {
+    serialMulta++;
   }
 
   public void cerrarConexion(){
